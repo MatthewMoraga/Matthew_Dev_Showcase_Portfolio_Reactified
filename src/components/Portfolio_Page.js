aed4785cc315745759fc5716938b2620bf567aae
MatthewM90 <matthewmoraga90@gmail.com>
2023-05-13 18:32:48 -0400

    portfolio page added

--- src/components/Portfolio_Page.js
+++ src/components/Portfolio_Page.js
@@ -1,10 +1,49 @@
 import React from "react";
+import Project from "./Project";
+import techDevBlog from "../images/blog_website_dashboard_page.jpg"
+import codeQuiz from "../images/question_page.png"
 import '../styles/Portfolio_Page.css';
+import "../styles/Project.css";
 
 function Portfolio() {
   return (
    <main>
+    <div className="protfolio-wrapper">
+      <h1 className="portfolio-title">Portfolio</h1>
+      <div className="portfolio-container">
+        {[
+          {
+            title: "Tech Dev Blog",
+            description: "MVC Dev Showcase",
+            imageSource: techDevBlog,
+            deployment: 
+            "https://immense-caverns-94261.herokuapp.com/",
+            githubLink:
+            "https://github.com/MatthewMoraga/MVC_Paradigm_Development_Blog",
+          },
+          {
+            title: "Code Quiz Game",
+            description: "Test Your Coding Knowledge",
+            imageSource: codeQuiz,
+            deployment: 
+            "https://matthewmoraga.github.io/code_quiz_game_v1/",
+            githubLink:
+            "https://github.com/MatthewMoraga/code_quiz_game_v1"
+          }
+        ].map((project, index) => (
+          <div key={index}>
+            <Project
+            title={project.title}
+            description={project.description}
+            imageSource={project.imageSource}
+            deployment={project.deployment}
+            githubLink={project.githubLink}
+            />
+          </div>
+        ))}
 
+      </div>
+    </div>
    </main>
   )
 }
---
+++ src/components/Project.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "../styles/Project.css";
+
+function Project (props) {
+
+    return (
+        <main>
+            <div className="card-section">
+                <div className="card-wrapper">
+                    <div className="card">
+                        <h1 className="card-title">{props.title}</h1>
+                        <a href={props.deployment} target="_blank" rel="nonreferrer">
+                            <img src={props.imageSource} alt={props.imgAlt}/>
+                        </a>
+                        <div className="card-body">
+                            <p className="card-text">{props.description}</p>
+                            <a 
+                            href={props.deployment}
+                            target="_blank"
+                            rel="nonreferrer"
+                            className="project-anchor hover-underline-animation"
+                            >
+                            Deployment
+                            </a>
+                            <a
+                            href={props.githubLink}
+                            target="_blank"
+                            rel="nonrefferer"
+                            className="project-anchor hover-underline-animation"
+                            >
+                            GitHub Repo
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+    )
+    
+    
+}
+export default Project;
